00367b27c2ecfbf152f055389a91b9af60f2806f
Rudolph Gottesheim <r.gottesheim@loot.at>
2018-07-23 11:59:36 +0200

    Add TranslatorInterface

---
+++ src/Translate/TranslatorInterface.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Eventjet\I18n\Translate;
+
+use Eventjet\I18n\Language\LanguagePriorityInterface;
+
+interface TranslatorInterface
+{
+    public function translate(string $message, LanguagePriorityInterface $languages): string;
+}
