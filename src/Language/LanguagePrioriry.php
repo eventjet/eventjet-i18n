5812b5c864262b384747563d5ef44a4e38d55e1d
Thomas Rieschl <thomas@trinet.at>
2022-01-11 10:15:55 +0100

    update code tools adapt to composer 2.2 normalize composer

--- src/Language/LanguagePrioriry.php
+++ src/Language/LanguagePrioriry.php
@@ -8,8 +8,8 @@ namespace Eventjet\I18n\Language;
  * LanguagePriority originally had this typo, so we keep this for backwards compatibility.
  *
  * @deprecated
+ * @phpstan-ignore-next-line
  */
 class LanguagePrioriry extends LanguagePriority
 {
-
 }
